3d1de9632f9cbdd1ff202adfec2f02ad225be9d9
James <Jatti045@uottawa.ca>
2024-10-25 23:01:56 -0400

    Created server side package and ServerConsole class within server side package which implements ChatIF interface

---
+++ simpleChat-master/edu/seg2105/server/ServerConsole.java
@@ -0,0 +1,10 @@
+package edu.seg2105.server;
+
+import edu.seg2105.client.common.ChatIF;
+
+public class ServerConsole implements ChatIF {
+    @Override
+    public void display(String message) {
+
+    }
+}
